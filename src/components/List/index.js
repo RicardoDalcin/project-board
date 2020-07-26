e46f0c5d61e40d7f8bd281e7f11316b5a6acf0f4
Ricardo <ricardo.dalcinh@gmail.com>
2020-07-26 19:34:48 -0300

    Drag and drop cards

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -4,7 +4,7 @@ import { MdAdd } from 'react-icons/md'
 import { Container } from './styles'
 import Card from '../Card'
 
-export default function List({ data }) {
+export default function List({ data, index: listIndex }) {
   return (
     <Container done={data.done}>
       <header>
@@ -17,8 +17,8 @@ export default function List({ data }) {
       </header>
 
       <ul>
-        {data.cards.map((card) => (
-          <Card key={card.id} data={card} />
+        {data.cards.map((card, index) => (
+          <Card key={card.id} index={index} data={card} listIndex={listIndex} />
         ))}
       </ul>
     </Container>
